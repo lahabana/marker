10fe23454fb96b8ba0a93825c194789223105f64
Charly MOLTER <charly.molter@ensimag.imag.fr>
2012-07-09 23:08:54 +0200

    adding some comments

--- marker.js
+++ marker.js
@@ -11,6 +11,7 @@
 		$(that).append(titleContainer);
 		
 		var catMainContainer = $("<ul/>").addClass(data.classMainCatContainer);
+		// We create the html for each category
 		$.each(data.categories, function(idx, val) {
 			var catContainer = $("<li>").addClass(data.classCatContainer);
 			$(catContainer).append($("<div>" + val.name + "</div>").addClass(data.classCatTitle));
@@ -19,7 +20,10 @@
 			data.total += val.mark;
 		});
 		$(that).append(catMainContainer);
+
+		// We add the total score
 		$(titleContainer).append($("<div>" + data.total + "</div>").addClass(data.classTotal));
+		// This is to put the left bar to the size of the right bar
 		$(titleContainer).height($(catMainContainer).height());
 	};
 
@@ -32,26 +36,27 @@
 	        	if (!$(this).data('marker')) {
 					$(this).data('marker', $.extend({
 						title: "",
-						categories: [],// each categorie is a json like: {name: "", mark: ""}
-						classMain: "marker-main",
-						classTitle: "marker-title",
-						classTotal: "marker-total",
-						classTitleContainer: "marker-title-container",
-						classCatContainer: "marker-cat",
+						categories: [],// each category is a json like: {name: "js", mark: 4}
+						classMain: "marker-main", // class added to the main element
+						classTitle: "marker-title", // class of the element that contains the text
+						classTotal: "marker-total", // class of the element that contains the total
+						classTitleContainer: "marker-title-container", // class of the element that contains the title and the mark
+						classCatContainer: "marker-cat", 
 						classMainCatContainer: "marker-cat-container",
 						classCatTitle: "marker-cat-name",
 						classMark: "marker-cat-mark",
-						total: 0
+						total: 0 // total number of points (sum of all mark for each category)
 					}, options));
+
+					// We create the actual html
+					createMarkup(this);
 				}
-				console.log($(this).data('marker'));
-				createMarkup(this);
+				
 	    	});
 		}
 	};
 
-	  $.fn.marker = function( method ) {
-	    
+	  $.fn.marker = function( method ) {	    
 	    if ( methods[method] ) {
 	      	return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
 	    } else if ( typeof method === 'object' || ! method ) {
